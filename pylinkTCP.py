b14d38693f402b7c502b36734e50dda9a4d922f3
Hannes <hanzese@gmail.com>
2017-06-07 01:48:49 +0300

    Update pylinkTCP.py
    Fixed some connection problems. Added timestamp info to emoncms   &timestamp

--- pylinkTCP.py
+++ pylinkTCP.py
@@ -1,9 +1,14 @@
-import  sys, string
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import string
 import httplib
+import urllib2
 import StringIO
 import time
-
+import datetime
 from time import sleep
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -12,56 +17,85 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
-# Domain you want to post to: localhost would be an emoncms installation on you$
+# Domain you want to post to: localhost would be an emoncms installation on your own laptop
 # this could be changed to emoncms.org to post to emoncms.org
-#domain = "localhost"
-
-# Location of emoncms in your server, the standard setup is to place it in a fo$
+# Location of emoncms in your server, the standard setup is to place it in a folder called emoncms
 # To post to emoncms.org change this to blank: ""
-emoncmspath = "emoncms"
+
+emoncmspath = 'emoncms'
 
 # Write apikey of emoncms account
-apikey = "6c1d3709c8767eca32b943f0e89978dc"
+apikey = '6c1d3709c8767eca32b943f0e89978dc'
 
 # Node id youd like the emontx to appear as
 nodeid = 5
+conn = httplib.HTTPConnection("192.168.1.3")
 
-
-conn = httplib.HTTPConnection("localhost")
-conn1 = httplib.HTTPConnection("192.168.1.20:80")
-
-
-
-# Set this to the serial port of your emontx and baud rate, 9600 is standard em$
-#ser = serial.Serial('/dev/ttyUSB1', 115200)
-
-
-while 1:
-print "Get data"
-  conn1.request("GET", "/realtime.csv")
-  r1 = conn1.getresponse()
-  sleep (30)
-  line = r1.read()
+test = "200"
+global data
+global MaxRetry
+MaxRetry = 5
+def con(MaxRetry):
+    print "Get data"
+    while (MaxRetry >= 0):
+        try:
+            r1 = urllib2.urlopen("http://192.168.1.20:80/realtime.csv",timeout = 1 )
+            r2 = r1.getcode()
+            print r2
+            global data
+            line = r1.read()
+            data = line
+            print "Inverter data: ", line
+            print "data muutuja", data
+            line = line.rstrip()
+            data = map(int, line.split(";"))
+            print "Print a: ", data
+            print "Esimene data:", data[0]
+            print "Teine data  :", data[1]
+            print "Komas data  :", data[2]
+            timestamp = time.strftime("%d:%m:%Y %H:%M:%S", time.gmtime(data[0]))
+            print "Timestamp  : ", timestamp
+            if r2 == test:
+               print "Response Fail_Fail_Fail"
+               sleep(5)
+               r1.close()
+               con(MaxRetry)
+            else:
+               print "Response OK OK OK"
+               sleep(1)
+               send()
+        except urllib2.URLError as e:
+            print (e)
+            print ("Ühenduse viga!! Proovin 5 korda veel:"), MaxRetry
+            time.sleep(5)
+            MaxRetry = MaxRetry - 1
+            print ("Connect to Internet and Retry Later ")
+            print(" ######--- Closing Connection -- ###### ")
+            r1.close()
+            exit()
 
 # Remove the new line at the end
-  line = line.rstrip()
-  conn1.close()
-  #print line
-  print(bcolors.OKBLUE + line + bcolors.ENDC)
+#  line = line.rstrip()
+# print line
 
-  # Split the line at the whitespaces
-  array = line.split(';')
+def send():
 
-  # Create csv string
-  csv = ",".join(array)
+# Split the line at the whitespaces
 
-  # Send to emoncms
+    b = ','.join(str(e) for e in data)
+    array = b.split(",")
+# Create csv string
+    ajastamp = str(data[0])
+    csv = ",".join(array)
+#csv = ','.join(array)
+    
+    print bcolors.OKBLUE + csv + bcolors.ENDC
+# Send to emoncms
+    print "Sending data"
+    print ('&node=' + str(nodeid) + '&timestamp=' + ajastamp + '&csv=' + csv)
+    conn.request("GET", "/"+emoncmspath+"/input/post.json?apikey="+apikey+"&node="+str(nodeid)+"&timestamp="+ajastamp+"&csv="+csv)
+    conn.close()
 
-  conn.request("GET", "/"+emoncmspath+"/input/post.json?apikey="+apikey+"&node="+str(nodeid)+"&csv="+csv)
-  conn.close()
-  #sleep (10)
-  #response = conn.getresponse()
-  #print response.read()
-  print "Ootan 40 sekundit"
-  sleep (10)
-#conn1.close()
+while 1:
+   sleep(1)
+   con(5)
